8a290dcfedfcd329aeaa58e9cb6b4a9758da4906
jnphilipp <mail@jnphilipp.org>
2017-11-30 20:48:38 +0100

    Init computer project.

---
+++ computer/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""computer is a conversational system and intelligent personal assistant."""
+
+
+__author__ = 'Nathanael Philipp'
+__copyright__ = 'Copyright 2017 Nathanael Philipp (jnphilipp)'
+__license__ = 'GPLv3'
+__maintainer__ = __author__
+__email__ = 'mail@jnphilipp.org'
+__app_name__ = 'computer'
+__version_info__ = (0, 1, 0)
+__version__ = '.'.join(str(e) for e in __version_info__)
+__description__ = 'computer is a conversational system and intelligent ' +
+                  'personal assistant.'
