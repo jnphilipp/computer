357c7ab47ab49a175dfb38f2721451de6f85d446
jnphilipp <mail@jnphilipp.org>
2017-11-30 20:58:21 +0100

    Fix computer module.

--- computer/__init__.py
+++ computer/__init__.py
@@ -10,5 +10,5 @@ __email__ = 'mail@jnphilipp.org'
 __app_name__ = 'computer'
 __version_info__ = (0, 1, 0)
 __version__ = '.'.join(str(e) for e in __version_info__)
-__description__ = 'computer is a conversational system and intelligent ' +
+__description__ = 'computer is a conversational system and intelligent ' + \
                   'personal assistant.'
