7622f5db7772461771f0e83553dd97bfabcc5bec
jnphilipp <mail@jnphilipp.org>
2017-12-01 23:26:26 +0100

    Add Intent model to intents app.

--- intents/models.py
+++ intents/models.py
@@ -1,5 +1,32 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class SingleLineTextField(models.TextField):
+    pass
+
+
+class Intent(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created at')
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('Updated at')
+    )
+
+    name = SingleLineTextField(
+        unique=True,
+        verbose_name=_('Content')
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = _('Intent')
+        verbose_name_plural = _('Intents')
