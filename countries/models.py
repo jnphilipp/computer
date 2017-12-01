ee85a54fbbcbcf0c09ba8a93cf8267ae3d10abfc
jnphilipp <mail@jnphilipp.org>
2017-12-01 12:56:31 +0100

    Add Country model to countries app.

--- countries/models.py
+++ countries/models.py
@@ -1,5 +1,46 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class SingleLineTextField(models.TextField):
+    pass
+
+
+class Country(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created at')
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('Updated at')
+    )
+
+    name = SingleLineTextField(
+        unique=True,
+        verbose_name=_('Name')
+    )
+    locale_name = SingleLineTextField(
+        unique=True,
+        verbose_name=_('Locale name')
+    )
+    code = models.CharField(
+        max_length=2,
+        unique=True,
+        verbose_name=_('ISO code')
+    )
+    phone_prefix = models.CharField(
+        max_length=5,
+        unique=True,
+        verbose_name=_('Phone prefix')
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('code',)
+        verbose_name = _('Country')
+        verbose_name_plural = _('Countries')
