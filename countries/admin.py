86626e39f868ae64f68a9b407e6c4f192e7f750c
jnphilipp <mail@jnphilipp.org>
2017-12-01 13:32:54 +0100

    Update countries admin.

--- countries/admin.py
+++ countries/admin.py
@@ -1,5 +1,40 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.forms import TextInput
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import Country, Language, SingleLineTextField
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'locale_name', 'code', 'phone_prefix']}),
+    ]
+    formfield_overrides = {
+        SingleLineTextField: {
+            'widget': TextInput(attrs={'autocomplete': 'off'})
+        },
+    }
+    list_display = ('name', 'locale_name', 'code')
+    search_fields = ('name', 'locale_name', 'code')
+    ordering = ('code',)
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'code']}),
+        (_('Countries'), {'fields': ['countries']}),
+    ]
+    filter_horizontal = ('countries',)
+    formfield_overrides = {
+        SingleLineTextField: {
+            'widget': TextInput(attrs={'autocomplete': 'off'})
+        },
+    }
+    list_display = ('name', 'code')
+    list_filter = ('countries',)
+    search_fields = ('name', 'code')
+    ordering = ('code',)
