44e32fd0c729d39accf003888f3bc05e12d63af4
jnphilipp <mail@jnphilipp.org>
2017-12-12 07:19:02 +0100

    Update intents models.

--- intents/admin.py
+++ intents/admin.py
@@ -1,111 +1,50 @@
 # -*- coding: utf-8 -*-
 
+from computer.fields import SingleLineTextField
 from django.contrib import admin
 from django.db.models import Count
 from django.forms import TextInput
 from django.utils.translation import ugettext_lazy as _
-from texts.models import SingleLineTextField as TSingleLineTextField, Text
+from texts.models import Trigger
 
-from .models import Answer, Attribute, Intent, SingleLineTextField
+from .models import Intent
 
 
-class AnswerInline(admin.TabularInline):
+class TriggerInline(admin.TabularInline):
     extra = 1
     fieldsets = [
-        (None, {
-            'fields':['text', 'language', 'intent', 'required_attributes']
-        }),
+        (None, {'fields': ['text', 'language', 'intent']}),
     ]
-    filter_horizontal = ('required_attributes',)
     formfield_overrides = {
         SingleLineTextField: {
             'widget': TextInput(attrs={
-                'autocomplete': 'off',
                 'style': 'min-width: 75%;'
             })
         },
     }
-    model = Answer
-
-
-class TextInline(admin.TabularInline):
-    extra = 1
-    fieldsets = [
-        (None, {'fields': ['content', 'language', 'intent']}),
-    ]
-    formfield_overrides = {
-        TSingleLineTextField: {
-            'widget': TextInput(attrs={
-                'autocomplete': 'off',
-                'style': 'min-width: 75%;'
-            })
-        },
-    }
-    model = Text
+    model = Trigger
 
 
 @admin.register(Intent)
 class IntentAdmin(admin.ModelAdmin):
+    def answers_count(self, inst):
+        return inst.answers.count()
+
+    answers_count.admin_order_field = 'answers_count'
+    answers_count.short_description = _('Number of Answers')
     fieldsets = [
         (None, {'fields': ['name']}),
+        (_('Answers'), {'fields': ['answers']})
     ]
     formfield_overrides = {
         SingleLineTextField: {
             'widget': TextInput(attrs={
-                'autocomplete': 'off',
                 'style': 'min-width: 50%;'
             })
         },
     }
-    list_display = ('name',)
-    inlines = (TextInline, AnswerInline)
-    search_fields = ('name',)
+    filter_horizontal = ('answers',)
+    inlines = (TriggerInline,)
+    list_display = ('name', 'answers_count')
     ordering = ('name',)
-
-
-@admin.register(Answer)
-class AnswerAdmin(admin.ModelAdmin):
-    def get_queryset(self, request):
-        return Answer.objects.annotate(Count('required_attributes'))
-
-    def required_attributes_count(self, inst):
-        return inst.required_attributes__count
-
-    fieldsets = [
-        (None, {'fields': ['text', 'language', 'intent']}),
-        (_('Attributes'), {'fields': ['required_attributes']}),
-    ]
-    filter_horizontal = ('required_attributes',)
-    formfield_overrides = {
-        SingleLineTextField: {
-            'widget': TextInput(attrs={
-                'autocomplete': 'off',
-                'style': 'min-width: 50%;'
-            })
-        },
-    }
-    list_display = ('text', 'language', 'intent', 'required_attributes_count')
-    list_filter = ('intent', 'language')
-    required_attributes_count.admin_order_field = 'required_attributes__count'
-    required_attributes_count.short_description = _('Number of Attributes')
-    search_fields = ('text', 'language__code', 'language__name',
-                     'intent__name')
-    ordering = ('intent', 'language')
-
-
-@admin.register(Attribute)
-class AttributeAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['key', 'value']}),
-    ]
-    formfield_overrides = {
-        SingleLineTextField: {
-            'widget': TextInput(attrs={
-                'autocomplete': 'off',
-                'style': 'min-width: 50%;'
-            })
-        },
-    }
-    list_display = ('key', 'value')
-    search_fields = ('key', 'value')
-    ordering = ('key', 'value')
+    search_fields = ('name',)
--- intents/models.py
+++ intents/models.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from computer.fields import SingleLineTextField
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
-
-class SingleLineTextField(models.TextField):
-    pass
 
 
 class Intent(models.Model):
@@ -22,6 +19,12 @@ class Intent(models.Model):
         unique=True,
         verbose_name=_('Name')
     )
+    answers = models.ManyToManyField(
+        'texts.Answer',
+        blank=True,
+        related_name='intents',
+        verbose_name=_('Answers')
+    )
 
     def __str__(self):
         return self.name
@@ -30,74 +33,3 @@ class Intent(models.Model):
         ordering = ('name',)
         verbose_name = _('Intent')
         verbose_name_plural = _('Intents')
-
-
-class Attribute(models.Model):
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name=_('Created at')
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True,
-        verbose_name=_('Updated at')
-    )
-
-    key = SingleLineTextField(
-        verbose_name=_('Key')
-    )
-    value = SingleLineTextField(
-        blank=True,
-        null=True,
-        verbose_name=_('Value')
-    )
-
-    def __str__(self):
-        return '%s: %s' % (self.key, self.value)
-
-    class Meta:
-        ordering = ('key', 'value')
-        unique_together = ('key', 'value')
-        verbose_name = _('Attribute')
-        verbose_name_plural = _('Attributes')
-
-
-class Answer(models.Model):
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name=_('Created at')
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True,
-        verbose_name=_('Updated at')
-    )
-
-    text = SingleLineTextField(
-        verbose_name=_('Text')
-    )
-    language = models.ForeignKey(
-        'countries.Language',
-        models.CASCADE,
-        related_name='answers',
-        verbose_name=_('Language')
-    )
-    intent = models.ForeignKey(
-        Intent,
-        models.CASCADE,
-        related_name='answers',
-        verbose_name=_('Intent')
-    )
-    required_attributes = models.ManyToManyField(
-        Attribute,
-        blank=True,
-        related_name='answers',
-        verbose_name=_('Required attributes')
-    )
-
-    def __str__(self):
-        return self.text
-
-    class Meta:
-        ordering = ('text',)
-        unique_together = ('text', 'language', 'intent')
-        verbose_name = _('Answer')
-        verbose_name_plural = _('Answers')
